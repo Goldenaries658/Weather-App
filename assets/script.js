aed616a04687fbeaa22fba652370c2759d1ee9fe
Goldenaries <jacob.houston658@gmail.com>
2020-04-25 17:44:01 +0100

    Created ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,17 @@
-$(function(){
-    $('#sumbit-button').on('click', function(e){
-        e.preventDefault();
-        
+$(function () {
+  $('#submit-button').on('click', function (event) {
+    event.preventDefault();
+    var city = $('#city-input').val();
+    var queryURL =
+      'http://api.openweathermap.org/data/2.5/weather?q=' +
+      city +
+      '&appid=23cadb273565bbb0f7aaa7a5b98a990a';
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET',
+    }).then(function (response) {
+      console.log(response);
     });
+  });
 });
