353a049933a66f198cec99fe9a9e82143313dd87
Goldenaries <jacob.houston658@gmail.com>
2020-04-25 21:37:50 +0100

    Displaying current weather data

--- assets/script.js
+++ assets/script.js
@@ -11,14 +11,29 @@ $(function () {
       url: queryURL,
       method: 'GET',
       error: function (xhr, ajaxOptions, thrownError) {
-        if (xhr.status == 400){
-            alert('400: Invalid Input.')
-        } else if(xhr.status == 404) {
-            alert('404: Not Found.')
+        if (xhr.status == 400) {
+          alert('400: Invalid Input.');
+        } else if (xhr.status == 404) {
+          alert('404: Not Found.');
         }
       },
     }).then(function (response) {
-      console.log(response);
+      var icon =
+        'http://openweathermap.org/img/wn/' +
+        response.weather[0].icon +
+        '@2x.png';
+      var temp = response.main.temp - 273.15;
+      var humidity = response.main.humidity + '%';
+      var windSpeed = response.wind.speed + ' m/s';
+      var cloudiness = response.clouds.all + '%';
+
+      $('#city-name').text(response.name);
+      $('#date').text(moment().format('MMMM Do YYYY, H:mm'));
+      $('#weather-icon').attr('src', icon);
+      $('#temp').text(temp.toFixed(2) + ' °C');
+      $('#humidity').text('Humidity: ' + humidity);
+      $('#wind-speed').text('Windspeed: ' + windSpeed);
+      $('#cloudiness').text('Cloudiness: ' + cloudiness);
     });
   });
 });
