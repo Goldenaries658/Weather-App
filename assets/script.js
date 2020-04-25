28a3a18b083cf75442e18b486b38db864ffc5ab5
Goldenaries <jacob.houston658@gmail.com>
2020-04-25 22:36:28 +0100

    Added saving history

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
 $(function () {
+  // Setting an index for history entries.
+  if (!localStorage.getItem('index')) {
+    localStorage.setItem('index', 0);
+  }
+
   $('#submit-button').on('click', function (event) {
     event.preventDefault();
     var city = $('#city-input').val();
@@ -7,6 +12,7 @@ $(function () {
       city +
       '&appid=23cadb273565bbb0f7aaa7a5b98a990a';
 
+    // Calling the Open Weather Map API
     $.ajax({
       url: queryURL,
       method: 'GET',
@@ -18,6 +24,27 @@ $(function () {
         }
       },
     }).then(function (response) {
+      // Saving to local storage and increasing index
+      var alreadyInHistory = 0;
+      for (i = 0; i < localStorage.length; i++) {
+        var historyItem = localStorage.getItem(localStorage.key(i));
+        if (queryURL == historyItem){
+            alreadyInHistory = 1;
+        }
+    }
+    
+    if (!alreadyInHistory) {
+        var index = localStorage.getItem('index');
+        localStorage.setItem(index, queryURL);
+        localStorage.setItem('index', parseInt(index) + 1);
+      }
+
+      // Hiding the search box
+      $('#search-form').hide();
+      $('#show-search-button').show();
+
+      // Setting all variables
+      var name = response.name;
       var icon =
         'http://openweathermap.org/img/wn/' +
         response.weather[0].icon +
@@ -27,7 +54,8 @@ $(function () {
       var windSpeed = response.wind.speed + ' m/s';
       var cloudiness = response.clouds.all + '%';
 
-      $('#city-name').text(response.name);
+      // Setting Display values
+      $('#city-name').text(name);
       $('#date').text(moment().format('MMMM Do YYYY, H:mm'));
       $('#weather-icon').attr('src', icon);
       $('#temp').text(temp.toFixed(2) + ' °C');
@@ -35,5 +63,12 @@ $(function () {
       $('#wind-speed').text('Windspeed: ' + windSpeed);
       $('#cloudiness').text('Cloudiness: ' + cloudiness);
     });
+  });
+
+  // Displaying search
+  $('#show-search-button').on('click', function (e) {
+    e.preventDefault();
+    $('#search-form').show();
+    $('#show-search-button').hide();
   });
 });
